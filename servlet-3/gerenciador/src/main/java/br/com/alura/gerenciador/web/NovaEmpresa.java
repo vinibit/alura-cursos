348da73c7d51403cacca27b979e90e511f178183
Vinicius Martins <vinibitmartins@gmail.com>
2016-07-28 12:36:54 -0300

    Criação de Servlet para inclusão de novas empresas.

---
+++ servlet-3/gerenciador/src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
@@ -0,0 +1,32 @@
+package br.com.alura.gerenciador.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.Empresa;
+import br.com.alura.gerenciador.dao.EmpresaDAO;
+
+/**
+ * Servlet implementation class NovaEmpresa
+ */
+@WebServlet("/novaEmpresa")
+public class NovaEmpresa extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		Empresa empresa = new Empresa(nome);
+		new EmpresaDAO().adiciona(empresa);
+		
+		PrintWriter writer = response.getWriter();
+		writer.println("<html><body><p>Empresa " + empresa.getNome() + " adicionada com sucesso!</p></body></html>");
+	}
+
+}
