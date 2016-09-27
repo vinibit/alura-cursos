07be4c35faf0d61e2fc6275554b3e4b1a445b011
Vinicius Martins <vinibitmartins@gmail.com>
2016-09-27 09:37:19 -0300

    Finalização do projeto do curso: - Refatoração da funcionalidade para calcular IMC; - Implementação de comportamento para o botão "Adcionar".

--- alura-js/projeto/calcula-imc.js
+++ alura-js/projeto/calcula-imc.js
@@ -1,26 +1,35 @@
-var pacientesTr = document.getElementsByClassName("paciente");
-
-for (var i = 0; i < pacientesTr.length; i++) {
-	var pacienteTr = pacientesTr[i];
-
-	var nomeTd = pacienteTr.getElementsByClassName("info-nome")[0];
-	var pesoTd = pacienteTr.getElementsByClassName("info-peso")[0];
-	var alturaTd = pacienteTr.getElementsByClassName("info-altura")[0];
-
+function montaPaciente(linha) {
 	var paciente = {
-		nome: nomeTd.textContent, 
-		peso: pesoTd.textContent,
-		altura: alturaTd.textContent,
-		pegaImc: function() {
-			if (this.altura <= 0) {
-				console.log("Não posso executar uma divisão por 0!");
-			} else {
-				return this.peso / (this.altura * this.altura);		
+			nome: linha.getElementsByClassName("info-nome")[0].textContent, 
+			peso: linha.getElementsByClassName("info-peso")[0].textContent,
+			altura: linha.getElementsByClassName("info-altura")[0].textContent,		
+			pegaImc: function() {
+				if (this.altura <= 0) {
+					console.log("Não posso executar uma divisão por 0!");
+				} else {
+					return this.peso / (this.altura * this.altura);		
+				}
 			}
-		}
-	};
+		};
+	return paciente;		
+}
 
-	var imcTd = pacienteTr.getElementsByClassName("info-imc")[0];
-	imcTd.textContent = paciente.pegaImc();
+function paraCadaLinha(linhas, executa) {
+	for (var i = 0; i < linhas.length; i++) {
+		linha = linhas[i];
+		executa(linha);
+	};	
+}
 
-};
+var botao = document.getElementById("calcula-imcs");
+botao.addEventListener("click", function() {
+	var linhas = document.getElementsByClassName("paciente");
+	paraCadaLinha(linhas, function(linha) {
+		var paciente = montaPaciente(linha);		
+		linha.getElementsByClassName("info-imc")[0].textContent = paciente.pegaImc();
+	})
+});
+botao.addEventListener("click", function() {
+	console.log("Calculando todos IMCs...");
+});
+
