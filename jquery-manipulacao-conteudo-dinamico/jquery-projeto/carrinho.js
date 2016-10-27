0772624896ebf433e5254927f62728dc42f9d97c
Vinicius Martins <vinibitmartins@gmail.com>
2016-10-27 12:29:36 -0200

    Exercícios da aula 5 e 6 Alterações referentes aos recursos aprendidos nas respectivas aulas.

--- jquery-manipulacao-conteudo-dinamico/jquery-projeto/carrinho.js
+++ jquery-manipulacao-conteudo-dinamico/jquery-projeto/carrinho.js
@@ -1,15 +1,33 @@
+var umaPropaganda = function() {
+    var propagandas = ["O que acha de comprar uma motocicleta?",
+               "O que acha de comprar uma lancha?",
+               "O que acha de comprar uma bicicleta?",
+               "O que acha de comprar uma carro?"
+               ];
+    var posicao = Math.floor(propagandas.length * Math.random());
+    var texto = propagandas[posicao];
+
+    var row = $("<tr>")
+        .addClass("propaganda")        
+        .append($("<td>"))
+        
+    row.find("td").attr("colspan", 6).text(texto);
+
+    return row;
+}
+
 var atualizaDados = function () {
-    var totalValor = 0;
-    var totalQuantidade = 0;
-
-    var rows = $("tbody").find("tr:visible");
-    for (i = 0; i < rows.length; i++) {
-        totalValor += parseFloat($(rows[i]).find(".sub-total").text());
-        totalQuantidade += parseInt($(rows[i]).find(".subquantidade").text());
-    }
-
-    $("#valor-total").text(totalValor);
-    $("#quantidade-de-itens").text(totalQuantidade);
+    $(".carrinho").each(function() {
+        var totalValor = 0;
+        var totalQuantidade = 0;
+        var rows = $(this).find(".row:visible");
+        rows.each(function() {
+            totalValor += parseFloat($(this).find(".sub-total").text());
+            totalQuantidade += parseInt($(this).find(".subquantidade").text());
+        });
+        $(this).find(".valor-total").text(totalValor);
+        $(this).find(".quantidade-de-itens").text(totalQuantidade);
+    });
 }
 
 var removeItem = function (event) {
@@ -20,15 +38,22 @@ var removeItem = function (event) {
 $(".remove-item").click(removeItem);
 
 var undo = function () {
-    $("tr:visible").removeClass("recuperado");
-    var hiddenRows = $("tr:hidden");
+    var carrinho = $(this).closest(".carrinho");
+    carrinho.find("tr:visible").removeClass("recuperado");
+    var hiddenRows = carrinho.find("tr:hidden");
     hiddenRows.addClass("recuperado");
     hiddenRows.show();
     atualizaDados();
 }
-$("#undo").click(undo);
+$(".undo").click(undo);
 
 var aposInicializado = function () {
     atualizaDados();
+    $(".carrinho").each(function() {
+        var carrinho = $(this);
+        carrinho.find("tr:nth-child(3n), tr:last").each(function() {
+             umaPropaganda().insertAfter($(this));
+        })
+    });    
 }
-$(aposInicializado);
+$(aposInicializado);    
