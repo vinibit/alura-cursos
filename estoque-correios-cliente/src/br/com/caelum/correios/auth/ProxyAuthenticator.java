6d8d609232d649a166260424c778b056e4918c37
Vinicius Martins <vinibitmartins@gmail.com>
2017-08-17 10:50:35 -0300

    Exercício (Desafio) de implementação de um cliente consumindo o serviço dos Correios.

---
+++ estoque-correios-cliente/src/br/com/caelum/correios/auth/ProxyAuthenticator.java
@@ -0,0 +1,18 @@
+package br.com.caelum.correios.auth;
+
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+
+public class ProxyAuthenticator extends Authenticator {
+	private String user; 
+	private String password;
+
+	public ProxyAuthenticator(String user, String password) {
+		this.user = user;
+		this.password = password;
+	}
+
+	protected PasswordAuthentication getPasswordAuthentication() {
+		return new PasswordAuthentication(user, password.toCharArray());
+	}
+}
