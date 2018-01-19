fc22600345700b841cfa0b5e1d7a639ca69f4ac8
Vini Martins <vinibitmartins@gmail.com>
2018-01-19 12:37:12 -0200

    Aula 03 - Finalizando o formulário de cadastro e redirecionando para a listagem de produtos

--- node-js/casadocodigo/app/routes/produtos.js
+++ node-js/casadocodigo/app/routes/produtos.js
@@ -5,7 +5,15 @@ module.exports = function (app) {
         var produtosDAO = new app.infra.ProdutosDAO(connection);
 
         produtosDAO.lista(function(err, results) {
-            res.render('produtos/lista', {lista: results});
+            res.format({
+                    html: function() {
+                        res.render('produtos/lista', {lista: results});
+                    },
+                    json: function() {
+                        res.json(results);
+                    }
+                }
+            )
         });
 
         connection.end();
@@ -13,6 +21,17 @@ module.exports = function (app) {
 
     app.get('/produtos/form', function (req, res) {
         res.render('produtos/form');
-    })
+    });
     
+    app.post('/produtos', function (req, res){ 
+        var produto = req.body;
+
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+        produtosDAO.salva(produto, function(erros, resultado) {
+            res.redirect('/produtos')
+        })
+
+        connection.end();
+    });
 }
--- node-js/casadocodigo/config/express.js
+++ node-js/casadocodigo/config/express.js
@@ -1,10 +1,13 @@
 var express = require('express');
-var load = require('express-load');    
+var load = require('express-load');
+var parser = require('body-parser');
 
 module.exports = function () {    
     var app = express();
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
+
+    app.use(parser.urlencoded({extended: true}));
 
     load('routes', {cwd: 'app'})
         .then('infra')
